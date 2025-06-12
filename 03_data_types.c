b71c5649d0cd2540952408cee27916db2eb153ce
Kishan Poojary <kishanpoojaryy@gmail.com>
2025-06-12 17:58:23 +0530

    learned data types and variable

---
+++ 03_data_types.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int a; // variable declaration
+    a = 6; // variable initialization
+    printf("the output of this program is %d", a);
+    return 0;
+}
---
+++ 05_comments.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+   // I am writing this just for fun
+    printf("hello world");
+    return 0;
+}
+
